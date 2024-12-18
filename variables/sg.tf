d34fb95fc536abc25519bf97f956e198b9b940f0
MachaSreenath <sreenathshree9@gmail.com>
2024-12-18 13:09:12 +0530

    terraform changes

--- variables/sg.tf
+++ variables/sg.tf
@@ -1,13 +1,3 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0b4f379183e5706b9" #devops-practice
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.roboshop-all.id] #This means list
-
-  tags = {
-    Name = "HelloTerraform"
-  }
-}
-
 resource "aws_security_group" "roboshop-all" { #this is terraform name, for terraform reference only
     name        = var.sg-name #this is for AWS
     description = var.sg-description
