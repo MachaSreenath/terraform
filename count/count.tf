5c9a6695b5491292f08a680c799ce6ebd0285ea0
MachaSreenath <sreenathshree9@gmail.com>
2024-12-20 19:06:02 +0530

    terraform changes

--- count/count.tf
+++ count/count.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "web" {
   #count = 11 # count.index is a special variable given by terraform 
   count = length(var.instance_names)
   ami           = var.ami_id #devops-practice
-  instance_type = "t2.micro"
+  instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
+
   tags = {
     Name = var.instance_names[count.index]
   }
--- count/variables.tf
+++ count/variables.tf
@@ -3,11 +3,6 @@ variable "instance_names" {
   default = ["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web"]
 }
 
-variable "instance_names_two" {
-  type = list
-  default = ["mongodb","mysql","shipping"]
-}
-
 variable "ami_id" {
   default = "ami-0b4f379183e5706b9"
 }
