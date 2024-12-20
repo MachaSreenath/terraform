0e5faa705ed0aeab938fd106278d427ae488bc92
MachaSreenath <sreenathshree9@gmail.com>
2024-12-20 19:06:12 +0530

    terraform changes

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_names" {
+  type = list
+  default = ["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web"]
+}
+
+variable "ami_id" {
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "zone_id" {
+  default = "Z016460536HW3XT7VUB95"
+}
+
+variable "domain_name" {
+  default = "forpractice.uno"
+}
