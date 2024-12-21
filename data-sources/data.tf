d788116e467d78f0a01dec59a0c0b7d984fb307e
MachaSreenath <sreenathshree9@gmail.com>
2024-12-21 19:06:48 +0530

    terraform changes

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -17,3 +17,23 @@ data "aws_ami" "centos8" {
       values = ["hvm"]
     }
 }
+
+data "aws_ami" "aws-linux-2" {
+    owners = ["amazon"]
+    most_recent = true
+
+    filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*"]
+    }
+
+    filter {
+      name = "root-device-type"
+      values = ["ebs"]
+    }
+
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+}
